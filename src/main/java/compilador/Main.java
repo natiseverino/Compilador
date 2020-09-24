743ea985389f5df4b3a86009188403c6f08c9f22
natiseverino <natiseverino96@gmail.com>
2020-09-24 02:36:51 -0300

    Constantes en main

--- src/main/java/compilador/Main.java
+++ src/main/java/compilador/Main.java
@@ -2,9 +2,15 @@ package compilador;
 
 public class Main {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RED = "\u001B[31m";
+
     public static void main(String[] args) {
         TablaSimbolos.init();
 
         AnalizadorLexico al = new AnalizadorLexico();
+        System.out.println();
     }
+
 }
